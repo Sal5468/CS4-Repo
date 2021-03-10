b68c51bd15a9fe4146baa852f50650361ce7bad3
Sal5468 <55513032+Sal5468@users.noreply.github.com>
2021-03-10 09:41:17 -0800

    Added Express examples 4

---
+++ ExpressExamples/ExpressEx4/index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var http = require("http");
+var logger = require("morgan")
+
+var app = express();
+
+app.use(logger("short"))//this is a function pointer using morgan
+
+app.use(function(req,res,next)
+{
+  var minute = (new Date()).getMinutes();
+  if (minute % 2 == 0)
+  {
+    next();
+  } else
+  {
+    res.statusCode = 403;
+    res.end("Server Error");
+  }
+})
+app.use(function(req,res)
+{
+  res.end("Authentication Success.  Password is missionvista");
+})
+
+http.createServer(app).listen(3000);
