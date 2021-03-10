0af25a9c3554da2c9457bae672ce4b595493460d
Sal5468 <55513032+Sal5468@users.noreply.github.com>
2021-03-10 10:01:46 -0800

    Update index.js

--- ExpressExamples/ExpressEx5/index.js
+++ ExpressExamples/ExpressEx5/index.js
@@ -2,12 +2,12 @@ var express = require("express");
 var http = require("http");
 var app = express();
 
-app.use(function(req,res,next)
+app.use(function(req,res,next)//gets called everytime you acces port
 {
   console.log("In comes a request to: " + req.url)
   next()
 })
-app.get("/",function(req,res)
+app.get("/",function(req,res)//get is based on the url if it matches it goes to that specific get request
 {
   res.end("Welcome to the home page")
 })
@@ -19,9 +19,9 @@ app.get("/favicon.ico",function(req,res)
 })
 app.get("/about",function(req,res)
 {
-  res.end("Welcome to the about page")
+  res.end("Welcome to the about page")// if comment out and then put a next then it will get to the below function
 })
-app.use(function(req,res)
+app.use(function(req,res)//no get rquests get called if this is called
 {
   res.statusCode = 404
   res.end("Error! File not found")
