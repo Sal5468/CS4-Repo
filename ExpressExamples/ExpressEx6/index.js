641676ab254c49b4ed4439317286629abbec8fc0
Sal5468 <55513032+Sal5468@users.noreply.github.com>
2021-03-11 10:22:55 -0800

    comments

--- ExpressExamples/ExpressEx6/index.js
+++ ExpressExamples/ExpressEx6/index.js
@@ -8,16 +8,16 @@ app.use(logger("short"))
 app.get("/",function(req,res)//get is based on the url if it matches it goes to that specific get request
 {
   console.log(path.resolve(__dirname,"index.html"))
-  res.sendFile(path.resolve(__dirname,"index.html"))
+  res.sendFile(path.resolve(__dirname,"index.html"))//server send index.html from server to client server dictates what we will see
   //res.end("Welcome to the home page")
 })
 let infoList = [{name:"Jill"},{name:"Jim"},{name:"John"},{name:"Jojo"}]
 app.get("/request",function(req,res)//get is based on the url if it matches it goes to that specific get request
 {
-  res.json(infoList[0])
+  res.json(infoList[0])//passing back to the client that particular json object
 })
 app.get("/another",function(req,res)//get is based on the url if it matches it goes to that specific get request
 {
-  res.json(infoList[1])
+  res.json(infoList[1])//passing back to the client that particular json object
 })
 app.listen(3000,function(){console.log("started on port 3000")})
