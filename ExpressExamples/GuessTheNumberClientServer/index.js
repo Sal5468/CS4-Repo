3a4c83400c2c7af14ba72d5e26e183857e7e6e0e
Sal5468 <145004342@vistausd.org>
2021-03-16 09:47:04 -0700

    GuessTheNumberClientServer

---
+++ ExpressExamples/GuessTheNumberClientServer/index.js
@@ -0,0 +1,31 @@
+let express = require('express');
+let path = require("path");
+
+const guessnum = require('./guessNum');
+
+let app = express();
+
+//req is info sending to server from client.
+//res is info sending to client from server.
+app.get("/",function(req,res) {
+    console.log(path.resolve(__dirname,"index.html"));
+    res.sendFile(path.resolve(__dirname,"index.html"));
+});
+
+app.get("/init",function(req,res) {
+//???    set the guess num min and max
+    res.json(null);
+});
+
+let retData = {info:0,numTries:0}     //javascript object
+app.get("/guess", function(req, res){
+    console.log("in server number chosen is " + req.query.guessNum);
+//???  check the the player's guess and return the hint.
+//???  return how many tries.
+    res.json(retData);
+});
+
+//below is a wrapper of http.createServer(requestHandler).listen(3000);
+app.listen(3000,function() {
+    console.log("started on port 3000");
+});
---
+++ ExpressExamples/GuessTheNumberClientServer/randomModule3.js
@@ -0,0 +1,25 @@
+let randomModule3 = function(minVal=1,maxVal=10) {
+    this.minValue = minVal;
+    this.maxValue = maxVal;
+}
+
+randomModule3.prototype.setMinMax = function(minVal,maxVal) {
+    this.minValue = minVal;
+    this.maxValue = maxVal;
+}
+
+randomModule3.prototype.getMin = function() {
+    return this.minValue;
+}
+randomModule3.prototype.getMax = function() {
+    return this.maxValue;
+}
+
+randomModule3.prototype.randomInteger = function() {
+    return (Math.floor((Math.random() * (this.getRange()) + this.minValue)));
+}
+randomModule3.prototype.getRange = function() {
+    return (this.maxValue+1-this.minValue);
+}
+
+module.exports = randomModule3;
