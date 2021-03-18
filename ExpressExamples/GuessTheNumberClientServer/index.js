a457e6c1b791bf94a5b33a222ca58efcc05278a4
Sal5468 <55513032+Sal5468@users.noreply.github.com>
2021-03-18 09:19:05 -0700

    tabbing changes

--- ExpressExamples/GuessTheNumberClientServer/index.js
+++ ExpressExamples/GuessTheNumberClientServer/index.js
@@ -8,16 +8,14 @@ var bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
-//req is info sending to server from client.
-//res is info sending to client from server.
-app.get("/",function(req,res) {
-    console.log(path.resolve(__dirname,"index.html"));
-    res.sendFile(path.resolve(__dirname,"index.html"));
+app.get("/",function(req,res)
+{
+  console.log(path.resolve(__dirname,"index.html"));
+  res.sendFile(path.resolve(__dirname,"index.html"));
 });
 
 app.post("/init",function(req,res)
 {
-//???    set the guess num min and max
   console.log(req.body.minVal)
   console.log(req.body.maxVal)
   guessnum.storeNum(parseInt(req.body.minVal), parseInt(req.body.maxVal))
@@ -30,18 +28,14 @@ app.post("/changeminmax",function(req,res)
   res.json({minVal:req.body.minVal,maxVal:req.body.maxVal});
 });
 
-let retData = {info:0,numTries:0}     //javascript object
+let retData = {info:0,numTries:0}
+
 app.get("/guess", function(req, res)
 {
-    console.log("in server number chosen is " + req.query.guessNum);
-//???  check the the player's guess and return the hint.
-      retData.info = guessnum.guessNum(req.query.guessNum)
-//???  return how many tries.
-      retData.numTries = guessnum.getNumTries()
-    res.json(retData);
+  console.log("in server number chosen is " + req.query.guessNum);
+  retData.info = guessnum.guessNum(req.query.guessNum)
+  retData.numTries = guessnum.getNumTries()
+  res.json(retData);
 });
 
-//below is a wrapper of http.createServer(requestHandler).listen(3000);
-app.listen(3000,function() {
-    console.log("started on port 3000");
-});
+app.listen(3000,function() {console.log("started on port 3000")});
