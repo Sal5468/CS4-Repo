e42fae5ffb0124516eae623766dd1178ca66b528
Sal5468 <145004342@vistausd.org>
2021-03-23 09:44:10 -0700

    Update index.js

--- ProcessEnvPort/index.js
+++ ProcessEnvPort/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 3000//there is no port variable so it stores in 3000
+app.use(function(req,res){
+  console.log("In comes a request to: " + req.url)
+  res.end("welcome to web prog")
+})
+
+var port = process.env.PORT  || 2500;//there is no port variable so it stores in 3000
 
 console.log(port)
+app.listen(port)
