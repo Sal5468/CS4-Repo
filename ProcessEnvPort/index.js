fa87ecd103a707f36057b83ffa7758b8405cdfe4
Sal5468 <55513032+Sal5468@users.noreply.github.com>
2021-03-24 09:33:38 -0700

    Update index.js

--- ProcessEnvPort/index.js
+++ ProcessEnvPort/index.js
@@ -6,7 +6,7 @@ app.use(function(req,res){
   res.end("welcome to web prog")
 })
 
-var port = process.env.PORT  || 2500;//there is no port variable so it stores in 3000
+var port = process.env.PORT  || 3000;//there is no port variable so it stores in 3000
 
 console.log(port)
 app.listen(port)
